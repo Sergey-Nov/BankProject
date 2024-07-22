38345588f5e82da6763f403d0a53dcfdbe7150aa
xJOHNJAx <illnino1313@mail.ru>
2024-07-22 22:57:42 +0300

    UI добавил и настроил

--- src/main/java/org/example/bankingapp/config/SecurityConfig.java
+++ src/main/java/org/example/bankingapp/config/SecurityConfig.java
@@ -1,18 +1,22 @@
+package org.example.bankingapp.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/api/accounts/[**").hasRole("USER")
@@ -22,20 +26,26 @@ public class SecurityConfig {
                 .formLogin()
                 .and()
                 .httpBasic();
-        return http.build();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        manager.createUser(User.withUsername("Serg")
+                .password(new BCryptPasswordEncoder().encode("5678"))
                 .roles("USER")
                 .build());
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
+        manager.createUser(User.withUsername("Alex")
+                .password(new BCryptPasswordEncoder().encode("4321"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("Den")
+                .password(new BCryptPasswordEncoder().encode("1234"))
                 .roles("ADMIN")
                 .build());
         return manager;
--- src/main/java/org/example/bankingapp/controller/AccountController.java
+++ src/main/java/org/example/bankingapp/controller/AccountController.java
@@ -1,31 +1,38 @@
 package org.example.bankingapp.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.example.bankingapp.model.Account;
+import org.example.bankingapp.model.Transaction;
 import org.example.bankingapp.request.AccountRequest;
 import org.example.bankingapp.request.TransferRequest;
 import org.example.bankingapp.request.WithdrawRequest;
-import org.example.bankingapp.model.Account;
-import org.example.bankingapp.model.Transaction;
 import org.example.bankingapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@RestController
+@Controller
 @RequestMapping("/api/accounts")
-@Tag(name = "Account Controller", description = "API для управления банковскими счетами")
 public class AccountController {
+
     @Autowired
     private AccountService accountService;
 
-    @Operation(summary = "Создание учетной записи")
+    @GetMapping
+    public String getAllAccounts(Model model) {
+        List<Account> accounts = accountService.getAllAccounts();
+        model.addAttribute("accounts", accounts);
+        return "account/index";
+    }
+
     @PostMapping
+    @ResponseBody
     public ResponseEntity<Account> createAccount(@RequestBody AccountRequest request) {
         try {
             Account account = accountService.createAccount(request.getRecipientName(), request.getPinCode());
@@ -35,14 +42,14 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Получение всех счетов по имени получателя")
-    @GetMapping
+    @GetMapping("/recipient")
+    @ResponseBody
     public ResponseEntity<List<Account>> getAccountsByRecipientName(@RequestParam String recipientName) {
         return ResponseEntity.ok(accountService.getAccountsByRecipientName(recipientName));
     }
 
-    @Operation(summary = "Получение счета по ID")
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
         try {
             return accountService.getAccountById(id)
@@ -53,8 +60,8 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Внесение средств на счет")
     @PostMapping("/{id}/deposit")
+    @ResponseBody
     public ResponseEntity<Void> deposit(@PathVariable Long id, @RequestBody BigDecimal amount) {
         try {
             accountService.deposit(id, amount);
@@ -64,8 +71,8 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Снятие средств со счета")
     @PostMapping("/{id}/withdraw")
+    @ResponseBody
     public ResponseEntity<Void> withdraw(@PathVariable Long id, @RequestBody WithdrawRequest request) {
         try {
             accountService.withdraw(id, request.getAmount(), request.getPinCode());
@@ -77,8 +84,8 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Перевод средств между счетами")
     @PostMapping("/{id}/transfer")
+    @ResponseBody
     public ResponseEntity<Void> transfer(@PathVariable Long id, @RequestBody TransferRequest request) {
         try {
             accountService.transfer(id, request.getToAccountId(), request.getAmount(), request.getPinCode());
@@ -90,8 +97,8 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Получение всех транзакций по ID счета")
     @GetMapping("/{id}/transactions")
+    @ResponseBody
     public ResponseEntity<List<Transaction>> getTransactions(@PathVariable Long id) {
         try {
             return ResponseEntity.ok(accountService.getTransactions(id));
@@ -100,14 +107,14 @@ public class AccountController {
         }
     }
 
-    @Operation(summary = "Получение всех счетов")
     @GetMapping("/all")
+    @ResponseBody
     public ResponseEntity<List<Account>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAllAccounts());
     }
 
-    @Operation(summary = "Получение всех счетов (только для администраторов)")
     @GetMapping("/admin/all")
+    @ResponseBody
     public ResponseEntity<List<Account>> getAllAccountsForAdmin() {
         return ResponseEntity.ok(accountService.getAllAccounts());
     }
